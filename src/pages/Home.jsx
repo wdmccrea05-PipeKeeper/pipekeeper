d07617bf1b3feb7d11814e1c178c09df585b884a
Warren McCrea <wmccrea@indario.com>
2025-12-30 21:01:33 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -144,18 +144,6 @@ export default function HomePage() {
     setShowTestingNotice(false);
   };
 
-  // Don't render anything while loading user data
-  if (!user) {
-    return (
-      <div className="min-h-screen bg-gradient-to-br from-[#1a2c42] via-[#243548] to-[#1a2c42] flex items-center justify-center">
-        <div className="text-center">
-          <div className="text-4xl mb-4">🔄</div>
-          <p className="text-[#e8d5b7]">Loading...</p>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
       {showOnboarding && (
