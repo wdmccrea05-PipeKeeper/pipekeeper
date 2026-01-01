2bddc61015234ced9bf02436e5b11897915194d3
Warren McCrea <wmccrea@indario.com>
2026-01-01 18:08:31 +0000

    File changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Layout from "../Layout";
+import Layout from "./Layout";
 import Home from "./Home";
 import Pipes from "./Pipes";
 import PipeDetail from "./PipeDetail";
