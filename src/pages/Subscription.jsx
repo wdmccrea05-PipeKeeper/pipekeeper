55701751e7ef2307e6d0f88361b52942e9588420
Warren McCrea <wmccrea@indario.com>
2026-01-06 21:57:50 +0000

    File changes

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -430,7 +430,7 @@ export default function SubscriptionPage() {
                   </Button>
 
                   <p className="text-xs text-center text-stone-500">
-                    Secure payment powered by Stripe. Cancel anytime.
+                    Secure payment processing. Cancel anytime.
                   </p>
                 </div>
               )}
