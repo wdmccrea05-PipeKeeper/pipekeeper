0f18600fb760a74401265d7b3f34ab7c214e3d4d
Warren McCrea <wmccrea@indario.com>
2026-01-07 04:03:54 +0000

    File changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import Layout from "../Layout.js";
+import Layout from "./Layout";
 import Home from "./Home";
 import Pipes from "./Pipes";
 import PipeDetail from "./PipeDetail";
