8f4948bc8043e80974cc59ba43d8e552d7dbd4d3
Warren McCrea <wmccrea@indario.com>
2026-01-20 22:01:22 +0000

    Manual code change

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -1,42 +1,58 @@
+import * as React from "react";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import { cn } from "@/lib/utils";
 
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
+const Tabs = TabsPrimitive.Root;
 
-import { cn } from "@/lib/utils"
+const TabsList = React.forwardRef(function TabsList(
+  { className, ...props },
+  ref
+) {
+  return (
+    <TabsPrimitive.List
+      ref={ref}
+      className={cn(
+        "inline-flex h-10 items-center justify-center rounded-xl bg-white/5 p-1 text-[#E0D8C8]/70 border border-[#E0D8C8]/10",
+        className
+      )}
+      {...props}
+    />
+  );
+});
 
-const Tabs = TabsPrimitive.Root
+const TabsTrigger = React.forwardRef(function TabsTrigger(
+  { className, ...props },
+  ref
+) {
+  return (
+    <TabsPrimitive.Trigger
+      ref={ref}
+      className={cn(
+        "inline-flex items-center justify-center whitespace-nowrap rounded-lg px-3 py-1.5 text-sm font-medium transition-all",
+        "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#A35C5C]/60 focus-visible:ring-offset-0",
+        "data-[state=active]:bg-[#A35C5C]/80 data-[state=active]:text-[#E0D8C8] data-[state=active]:shadow",
+        "data-[state=inactive]:text-[#E0D8C8]/70 data-[state=inactive]:hover:bg-white/5 data-[state=inactive]:hover:text-[#E0D8C8]",
+        className
+      )}
+      {...props}
+    />
+  );
+});
 
-const TabsList = React.forwardRef(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={cn(
-      "inline-flex h-auto items-center justify-center bg-transparent border-b border-[#E0D8C8]/20 p-0",
-      className
-    )}
-    {...props} />
-))
-TabsList.displayName = TabsPrimitive.List.displayName
+const TabsContent = React.forwardRef(function TabsContent(
+  { className, ...props },
+  ref
+) {
+  return (
+    <TabsPrimitive.Content
+      ref={ref}
+      className={cn(
+        "mt-4 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#A35C5C]/60 focus-visible:ring-offset-0",
+        className
+      )}
+      {...props}
+    />
+  );
+});
 
-const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap px-4 py-2.5 text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#A35C5C]/60 focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50 text-[#E0D8C8]/70 data-[state=active]:bg-[#A35C5C]/80 data-[state=active]:text-[#E0D8C8] data-[state=active]:shadow-sm rounded-t-lg border-b-2 border-transparent data-[state=active]:border-[#A35C5C] min-h-[44px]",
-      className
-    )}
-    {...props} />
-))
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
-
-const TabsContent = React.forwardRef(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      className
-    )}
-    {...props} />
-))
-TabsContent.displayName = TabsPrimitive.Content.displayName
-
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export { Tabs, TabsList, TabsTrigger, TabsContent };
