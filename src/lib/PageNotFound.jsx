b5156f1e817663ca85587da307364cda7899b994
wdmccrea05-PipeKeeper <wdmccrea05@gmail.com>
2026-02-23 15:54:23 -0500

    Fix import path for useTranslation in PageNotFound

--- src/lib/PageNotFound.jsx
+++ src/lib/PageNotFound.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
-import { useTranslation } from '@/components/i18n/safeTranslation';
+import { useTranslation } from "@/components/i18n";
 
 
 export default function PageNotFound({}) {
