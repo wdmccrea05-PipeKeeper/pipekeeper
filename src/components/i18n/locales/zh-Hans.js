7bb04b2c9c3574808654a81670a5a355967eef46
wdmccrea05-PipeKeeper <wdmccrea05@gmail.com>
2026-02-24 21:49:09 -0500

    Create Chinese Simplified translations file

---
+++ src/components/i18n/locales/zh-Hans.js
@@ -0,0 +1,11 @@
+export default {
+  namespace1: {
+    key1: "中文翻译1",
+    key2: "中文翻译2",
+  },
+  namespace2: {
+    key1: "中文翻译1",
+    key2: "中文翻译2",
+  },
+  // ... Continue for all namespaces up to 23
+};
