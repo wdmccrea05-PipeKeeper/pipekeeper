86d2f50c5a939aa97f7d54bed652d07dc3d68618
wdmccrea05-PipeKeeper <wdmccrea05@gmail.com>
2026-03-18 12:14:17 -0400

    Add moduleAssets.js with module icons and function

---
+++ src/components/branding/moduleAssets.js
@@ -0,0 +1,12 @@
+export const MODULE_ICONS = {
+  collectionKeeper: 'https://media.base44.com/images/public/694956e18d119cc497192525/b9b1fc2c7_CollectionKeeperUpdated.png',
+  pipekeeper: 'https://media.base44.com/images/public/694956e18d119cc497192525/27f5c2c92_PKNB.png',
+  whiskeykeeper: 'https://media.base44.com/images/public/694956e18d119cc497192525/752a8ab5c_WKNB.png',
+  winekeeper: 'https://media.base44.com/images/public/694956e18d119cc497192525/ef580a0c9_WineKNB.png',
+  cigarkeeper: 'https://media.base44.com/images/public/694956e18d119cc497192525/c26fb6746_CigarKNB.png',
+  curator: 'https://media.base44.com/images/public/694956e18d119cc497192525/58275e6de_ChatGPT_Image_Mar_11__2026__03_30_13_PM-removebg-preview.png',
+};
+
+export function getModuleIcon(moduleId) {
+  return MODULE_ICONS[moduleId] || MODULE_ICONS.collectionKeeper;
+}
