cbfe3e583f4033ac02184e92eeb56acbf108e451
wdmccrea05-PipeKeeper <wdmccrea05@gmail.com>
2026-03-24 10:40:46 -0400

    Update PipeIcon SVG dimensions and path data

--- src/components/icons/PipeIcon.jsx
+++ src/components/icons/PipeIcon.jsx
@@ -7,53 +7,17 @@ export default function PipeIcon({
 }) {
   return (
     <svg
-      viewBox="0 0 64 64"
+      viewBox="0 0 1200 1200"
       className={className}
       role="img"
       aria-label={title}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
+      style={{ display: "block", color }}
     >
       <path
-        d="M18 14h10c6 0 10 4 10 10v6c0 7-5 12-12 12h-2c-7 0-12-5-12-12v-4c0-7 5-12 12-12Z"
-        stroke={color}
-        strokeWidth="3.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={color}
-        fillOpacity="0.18"
-      />
-      <path
-        d="M26 14v-4"
-        stroke={color}
-        strokeWidth="2.5"
-        strokeLinecap="round"
-      />
-      <path
-        d="M22 14v-3"
-        stroke={color}
-        strokeWidth="2.5"
-        strokeLinecap="round"
-      />
-      <path
-        d="M38 31h9c7 0 13 5 13 12v1"
-        stroke={color}
-        strokeWidth="3.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M60 44c0 4.5-3.5 8-8 8H36"
-        stroke={color}
-        strokeWidth="3.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M46 52h8"
-        stroke={color}
-        strokeWidth="3.5"
-        strokeLinecap="round"
+        d="M 45 239 L 40 253 L 40 306 L 57 333 L 88 339 L 115 319 L 172 319 L 207 325 L 268 355 L 317 399 L 385 489 L 509 696 L 569 783 L 635 857 L 717 918 L 768 941 L 830 956 L 902 959 L 960 953 L 1011 940 L 1059 917 L 1097 886 L 1125 848 L 1142 811 L 1154 764 L 1159 662 L 1146 506 L 1119 360 L 720 360 L 701 497 L 694 599 L 675 599 L 634 585 L 593 555 L 440 371 L 390 323 L 336 285 L 245 249 L 194 241 L 115 240 L 88 220 L 67 221 Z"
+        fill="currentColor"
       />
     </svg>
   );
